e7290644217483a0d7b3c1d38882886739297dda
Noor <74349478+bsnoor@users.noreply.github.com>
2023-03-01 15:52:14 +0200

    Update if_else_if.sh
    UPDATE Q2

--- if_else_if.sh
+++ if_else_if.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#Q1
 read -p "please enter a number : " num
 if [ $num -eq 1 ]; then
 echo "the correct number was entered"
 else
      echo "fail"
+fi
+
+#Q2
+read -p "please enter an argument : " arg
+if [  ! -z $arg  ]; then
+echo $arg
 fi
 
 
