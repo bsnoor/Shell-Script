f8bbc5d6e5c410896021182f504428aee73abb46
Noor <74349478+bsnoor@users.noreply.github.com>
2023-03-02 12:16:10 +0200

    Update Input_output.sh

--- Input_output.sh
+++ Input_output.sh
@@ -4,11 +4,18 @@ echo "hello , how are you ? "
 echo "hello have a nu=ice day "
 echo "hello this is my username : $hostname "
 echo "please enter an argument $USER "
-#Q1
+#Q4
 for i in red yellow green
 do 
  sleep 2
  echo $i
 done
+#Q5
+count=0
+for i in {1..4}
+do
+count=$(( count+$i ))
+done
+echo $count 
 
 
