7bf47658a8d85e5c9cbd74bcf8f91285debabe0a
Noor <74349478+bsnoor@users.noreply.github.com>
2023-03-02 12:05:17 +0200

    Update Input_output.sh

--- Input_output.sh
+++ Input_output.sh
@@ -4,3 +4,11 @@ echo "hello , how are you ? "
 echo "hello have a nu=ice day "
 echo "hello this is my username : $hostname "
 echo "please enter an argument $USER "
+#Q1
+for i in red yellow green
+do 
+ sleep 2
+ echo $i
+done
+
+
