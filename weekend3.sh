8e156bf49ee4be6017c0b3e807771e18d53b570f
Noor <74349478+bsnoor@users.noreply.github.com>
2023-03-04 15:03:46 +0200

    Update weekend3.sh

--- weekend3.sh
+++ weekend3.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+#!/bin/bash
+function day_of_the_week()
+{
+read -p "please enter a number between 1 and 7 : " num1
+case $num1 in
+   1)
+     echo "sunday"
+    ;;
+   2)
+     echo "monday"
+    ;;
+   3)
+    echo "tuesday" 
+    ;;
+   4)
+    echo "wednesday"
+    ;;
+   5)
+     echo "thursday" 
+    ;;
+   6)
+    echo "friday"
+    ;; 
+   7)
+    echo "saturday"
+    ;;
+  *)
+   echo "invalid input"
+   ;; 
+esac
+}
+
+day_of_the_week
