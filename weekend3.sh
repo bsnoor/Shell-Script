5fb80c0080ac0f51b950ecefa3edcba0e3f2e6e3
Noor <74349478+bsnoor@users.noreply.github.com>
2023-03-04 20:13:27 +0200

    Update weekend3.sh

--- weekend3.sh
+++ weekend3.sh
@@ -108,7 +108,22 @@ while [[ $i -ge 1 ]]; do
  echo " "
  ((i--))
 done
-
+#pyramid
+read -p "please enter a number " num
+i=1
+while [[ $i -le $num ]]; do
+  j=$num
+  while [[ $j -ge 1 ]]; do
+    if [[ $j -gt $i ]]; then
+        echo -n " "
+    else
+        echo -n " *"
+    fi
+    ((j--))
+  done
+  echo "  "
+  ((i++))
+done
 
 
 
